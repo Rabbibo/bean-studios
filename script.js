f9e13fc4e487ef9f51040d19fcf77b2e655cfe6b
SuperNoob413 <165925697+SuperNoob413@users.noreply.github.com>
2024-04-18 11:57:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,42 @@
-const body = document.querySelector('body')
-const label = document.querySelector('#label')
+let dvd = document.getElementById('dvd');
+let interval_id;
+let x_incr = 1;
+let y_incr = 1;
 
-let colors = [ '#26de81', '#fc5c65', '#fd9644', '#fed330', '#2bcbba', '#45aaf2', '#4b7bec', '#a55eea', '#ffc1f3', '#76ead7', '#ff9c71', '#32e0c4', '#d291bc', '#fa744f' ]
-
-let FPS = 60
-
-let width
-  , height
-  , velocityX = 1
-  , velocityY = 1
-  , pause = true
-  , previousColor = 0
-;
-
-setInterval(() => {
-  if (pause) return;
-
-  let rect = label.getBoundingClientRect()
-
-  let left = rect.x
-  let top = rect.y
-
-  if (left + rect.width >= width || left <= 0) {
-    velocityX = -velocityX
-    let randomColor = getRandomColor()
-    label.style.stroke = randomColor
-    
-    if (left + 150 <= width / 2) {
-      body.style.boxShadow = `inset 4px 0px 0px 0px ${randomColor}`
-    } else {
-      body.style.boxShadow = `inset -4px 0px 0px 0px ${randomColor}`
-    }
-  }
-  if (top + rect.height >= height || top <= 0) {
-    velocityY = -velocityY
-    let randomColor = getRandomColor()
-    label.style.stroke = randomColor
-    
-    if (top + 28 <= height / 2) {
-      body.style.boxShadow = `inset 0px 4px 0px 0px ${randomColor}`
-    } else {
-      body.style.boxShadow = `inset 0px -4px 0px 0px ${randomColor}`
-    }
-  }
-
-  label.style.left = rect.x + velocityX + 'px'
-  label.style.top = rect.y + velocityY + 'px'
-}, 1000 / FPS)
-
-
-const reset = () => {
-  width =
-    window.innerWidth ||
-    document.documentElement.clientWidth ||
-    document.body.clientWidth
-  ;
-
-  height =
-    window.innerHeight ||
-    document.documentElement.clientHeight ||
-    document.body.clientHeight
-  ;
-
-  pause =
-    width <= label.getBoundingClientRect().width ||
-    height <= label.getBoundingClientRect().height
-  ;
-
-  label.style.left = 'calc(50vw - 150px)'
-  label.style.top = 'calc(50vh - 28px)'
-  label.style.stroke = colors[0]
+function init() {
+  update_color();
+  dvd.style.position = 'absolute';
+  document.body.style.background = '#4d4d4d';
+  setInterval(frame, 5);
 }
 
-
-const getRandomColor = () => {
-  let currentColor = -1
-  
-  do {
-    currentColor = Math.floor(Math.random() * colors.length);
-  } while (previousColor == currentColor);
-  
-  previousColor = currentColor
-  
-  return colors[currentColor]
+function update_color() {
+  let color = Math.round((Math.random() * 100));
+  dvd.style.fill = `hsl(${color},100%,50%)`;
 }
 
-reset()
+function handle_collision() {
+  let dvd_height = dvd.offsetHeight;
+  let dvd_width = dvd.offsetWidth;
+  let left = dvd.offsetLeft;
+  let top = dvd.offsetTop;
+  let win_height = window.innerHeight;
+  let win_width = window.innerWidth;
 
-window.addEventListener('resize', reset, true)
+  if (left <= 0 || left + dvd_width >= win_width) {
+    x_incr = ~x_incr + 1;
+    update_color();
+  }
+  if (top <= 0 || top + dvd_height >= win_height) {
+    y_incr = ~y_incr + 1;
+    update_color();
+  }
+}
+
+function frame() {
+  handle_collision();
+  dvd.style.top = dvd.offsetTop + y_incr;
+  dvd.style.left = dvd.offsetLeft + x_incr;
+}
+
+init();
